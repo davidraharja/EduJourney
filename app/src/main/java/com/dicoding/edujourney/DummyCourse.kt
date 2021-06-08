8ba03c712074bdd91e29c492edc2fa7f9c4ca0c0
David <david.535180024@stu.untar.ac.id>
2021-06-08 10:03:01 +0700

    change data

--- app/src/main/java/com/dicoding/edujourney/DummyCourse.kt
+++ app/src/main/java/com/dicoding/edujourney/DummyCourse.kt
@@ -9,14 +9,14 @@ object DummyCourse {
     )
 
     private var img = arrayOf(
-        R.drawable.comms,
-        R.drawable.office,
-        R.drawable.english,
-        R.drawable.computing,
-        R.drawable.cwriting,
-        R.drawable.webdev,
-        R.drawable.accounting,
-        R.drawable.graphic
+        "https://www.freepik.com/free-vector/work-chat-concept-illustration_7118068.htm#page=1&query=communication&position=22",
+        "https://www.freepik.com/free-icon/office_14840978.htm#page=1&query=microsoft%20office&position=7",
+        "https://www.freepik.com/free-vector/you-speak-english-lettering-background_2619959.htm#page=1&query=english&position=2",
+        "https://www.freepik.com/free-vector/computer-monitor-graphic-animator-creating-video-game-modeling-motion-processing-video-file-using-professional-editor-vector-illustration-graphic-design-art-designer-workplace-concept_11671412.htm#page=1&query=computer&position=0",
+        "https://www.freepik.com/free-vector/wall-post-concept-illustration_7769804.htm#page=1&query=copywriting&position=1",
+        "https://www.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_10798281.htm#page=1&query=web%20development&position=1",
+        "https://www.freepik.com/free-vector/accountant-concept-illustration_13766143.htm#page=1&query=accounting&position=0",
+        "https://www.freepik.com/free-vector/graphic-design-colorful-geometrical-lettering_7088583.htm#page=1&query=graphic%20design&position=49"
     )
     private var title = arrayOf(
         "Communication",
@@ -99,7 +99,7 @@ object DummyCourse {
         for(position in id.indices){
             list.add(CourseResponse(
                 courseId = id[position],
-                image = img[position].toString(),
+                image = img[position],
                 title = title[position],
                 description = description[position],
                 type = type[position],
